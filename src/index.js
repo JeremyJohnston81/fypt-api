6426dc6c9f766a1b0d1818c0118ea472cd97d97f
Kevin Whitley <krwhitley@gmail.com>
2023-04-14 12:24:10 -0500

    migrate to itty-router@next

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-import { Router } from "itty-router";
-import { json, withParams } from "itty-router-extras";
+import {
+  createCors,
+  json,
+  Router,
+  withParams
+} from "itty-router";
 import HcadStore from "./stores/hcadStore";
 
 const router = Router();
 const hcad = new HcadStore();
-
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Content-Type": "application/json",
-};
-
-router.options("*", async () => {
-  const optionHeader = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, Content-Type, Accept",
-  };
-  return new Response(null, { status: 204, headers: optionHeader });
+const { preflight, corsify } = createCors({
+  methods: ["GET", "POST"]
 });
 
-router.get("/search/:address", withParams, async ({ address }) =>
-  json(await hcad.getAccountID(decodeURI(address)), { headers })
-);
+router
+  .all("*", preflight, withParams)
 
-router.get("/property/:account", withParams, async ({ account }) =>
-  json(await hcad.getProperty(account), { headers })
-);
+  .get("/search/:address", ({ address }) =>
+    hcad.getAccountID(decodeURI(address))
+  )
 
-router.get("/comps/:account", withParams, async ({ account }) =>
-  json(await hcad.getComps(account), { headers })
-);
+  .get("/property/:account", ({ account }) =>
+    hcad.getProperty(account)
+  )
 
-router.get("/lastUpdated", async ({}) =>
-  json(await hcad.getLastUpdated(), { headers })
-);
+  .get("/comps/:account", ({ account }) =>
+    hcad.getComps(account)
+  )
+
+  .get("/lastUpdated", () =>
+    hcad.getLastUpdated()
+  )
 
 // attach the router "handle" to the event handler
 addEventListener("fetch", (event) =>
-  event.respondWith(router.handle(event.request))
+  event.respondWith(
+    router
+      .handle(event.request)
+      .then(json) // transform raw data into JSON
+      .catch(error) // catch errors BEFORE corsify
+      .then(corsify) // corsify all Responses
+  )
 );
