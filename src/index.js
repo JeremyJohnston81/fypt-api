75df9a26d475a314b43e98ea1e3cc3270acac761
Jeremy Johnston <johnston.jeremy@gmail.com>
2023-04-18 14:01:20 -0400

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import {
-  createCors,
-  json,
-  Router,
-  withParams
-} from "itty-router";
+import { createCors, error, json, Router, withParams } from "itty-router";
 import { version } from "../package.json";
 import HcadStore from "./stores/hcadStore";
 
 const router = Router();
 const hcad = new HcadStore();
 const { preflight, corsify } = createCors({
-  methods: ["GET", "POST"]
+  methods: ["GET", "POST"],
 });
 
 router
@@ -26,19 +21,13 @@ router
   )
 
   // GET /property/:account - finds the property for a given account
-  .get("/property/:account", ({ account }) =>
-    hcad.getProperty(account)
-  )
+  .get("/property/:account", ({ account }) => hcad.getProperty(account))
 
   // GET /comps/:account - finds the comps for a given account
-  .get("/comps/:account", ({ account }) =>
-    hcad.getComps(account)
-  )
+  .get("/comps/:account", ({ account }) => hcad.getComps(account))
 
   // GET /lastUpdated - returns the date the HCAD store was last updated
-  .get("/lastUpdated", () =>
-    hcad.getLastUpdated()
-  )
+  .get("/lastUpdated", () => hcad.getLastUpdated());
 
 // attach the router "handle" to the event handler
 addEventListener("fetch", (event) =>
